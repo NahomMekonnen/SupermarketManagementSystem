834d0f988d1545395185dc1dc894bef6c86d0a4a
Natanim Mekonnen <mekonnennatanim1@gmail.com>
2023-09-13 01:09:52 +0300

    gui2 commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import database.Database;
 import employee.Employee;
 import inventory.Inventory;
 import supplier.Supplier;
+import login.*;
 
 import javax.swing.*;
 
@@ -9,15 +10,15 @@ public class Main {
 
 
     public static void main(String[] args) {
-//     Login l=new Login();
-//     l.setVisible(true);
+     Login l=new Login();
+     l.setVisible(true);
 
-        Database database =new  Database("localhost","SuperMarket","sa","Nohom420");
+        Database database =new  Database("localhost","SuperMarket","sa","Nani2239");
         database.instantiate();
 //        Inventory inventory=new Inventory();
 //        inventory.setVisible(true);
-        Supplier supplier=new Supplier();
-        supplier.setVisible(true);
+//        Supplier supplier=new Supplier();
+//        supplier.setVisible(true);
 
 //        Admin ad=new Admin();
 //        ad.setVisible(true);
--- src/supplier/Supplier.java
+++ src/supplier/Supplier.java
@@ -1,9 +1,11 @@
 package supplier;
 
+import admin.Admin;
 import database.Database;
 import inventory.InventoryActions;
 import product.Product;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
@@ -14,6 +16,9 @@ import javax.swing.table.TableRowSorter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 
 public class Supplier extends JFrame{
@@ -32,11 +37,11 @@ public class Supplier extends JFrame{
     SupplierActions supplierActions;
     JPanel p1,p2;
     JLabel l1,l2;
-    JTextField tf,tf2;
-    JTextArea TArea;
+    JTextField textField;
+
     JComboBox Cb;
     JTable T;
-    JButton newSupplier,supplyProducts,removeSupplier;
+    JButton newSupplier,supplyProducts,removeSupplier,back;
     JScrollPane scroll;
     JDialog newSupply,prodSupply,removeSupply;
     String[] options= {"ID","Name"};
@@ -44,21 +49,27 @@ public class Supplier extends JFrame{
         Container Sup = getContentPane();
         supplierActions = new SupplierActions();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(1000,600);
+        setSize(800,600);
         setLayout(null);
         setLocationRelativeTo(null);
         connection = Database.connection;
-        tf=new JTextField(100);
-        tf2=new JTextField(100);
+        textField=new JTextField(100);
 
-        tf.setBounds(400,0,300,50);
-        tf2.setBounds(400,400,300,50);
+
+        textField.setBounds(310,25,300,30);
+
         int count = supplierActions.Count();
         System.out.println(count);
         Supplier[] suppliers= supplierActions.Retrieve(count);
 
         Cb= new JComboBox();
+        Cb.addItem("ID");
+        Cb.addItem("Name");
+        Cb.setBounds(200,25,100,30);
         l1 = new JLabel("Search item using: ");
+        l1.setBounds(50,-10,50,800);
+        l1.setForeground(Color.WHITE);
+        l1.setFont(new Font("Arial", Font.BOLD, 16));
 
 
         String[] cNames = new String[]{"ID", "FirstName", "LastName", "Phone_No", "Item_Supplied", "Address"};
@@ -78,28 +89,23 @@ public class Supplier extends JFrame{
         T= new JTable(model);
         T.setRowSorter(sorter);
         scroll=new JScrollPane(T);
-        Cb.addItem("ID");
-        Cb.addItem("Name");
 
 
-        newSupplier= new JButton("Add Supplier");
-        supplyProducts= new JButton("Restock");
-        removeSupplier=new JButton("Remove Supplier");
 
-        tf.getDocument().addDocumentListener(new DocumentListener() {
+        textField.getDocument().addDocumentListener(new DocumentListener() {
             @Override
             public void insertUpdate(DocumentEvent e) {
-                search(tf.getText());
+                search(textField.getText());
             }
 
             @Override
             public void removeUpdate(DocumentEvent e) {
-                search(tf.getText());
+                search(textField.getText());
             }
 
             @Override
             public void changedUpdate(DocumentEvent e) {
-                search(tf.getText());
+                search(textField.getText());
             }
             private void search(String str){
                 String selected=(String)Cb.getSelectedItem();
@@ -107,17 +113,79 @@ public class Supplier extends JFrame{
                     if (str.length() == 0) {
                         sorter.setRowFilter(null);
                     } else {
-                        RowFilter rowFilter= RowFilter.regexFilter(tf.getText(),0);
+                        RowFilter rowFilter= RowFilter.regexFilter(textField.getText(),0);
                         sorter.setRowFilter(rowFilter);
                     }
                 }else if(selected.equals("Name")){
                     if (str.length() == 0) {
                         sorter.setRowFilter(null);
                     } else {
-                        RowFilter rowFilter= RowFilter.regexFilter(tf.getText(),1);
+                        RowFilter rowFilter= RowFilter.regexFilter(textField.getText(),1);
                         sorter.setRowFilter(rowFilter);
                     }
                 }
+            }
+        });
+
+        back=new JButton("Back");
+        back.setBackground(new Color(40,40,40));
+        back.setForeground(Color.WHITE);
+
+        back.setFocusPainted(false);
+        back.setFont(new Font("Arial", Font.BOLD, 16));
+
+        back.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                back.setBackground(new Color(60,60,60));
+            }
+
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                back.setBackground(new Color(40,40,40));
+            }
+        });
+
+        back.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(e.getSource()==back)
+                {
+                    dispose();
+                    Admin n=new Admin();
+                    n.setVisible(true);
+                }
+
+            }
+        });
+        back.setBounds(10,10,80,20);
+
+
+        newSupplier= new JButton("Add Supplier");
+        newSupplier.setBackground(new Color(40,40,40));
+        newSupplier.setForeground(Color.WHITE);
+
+        newSupplier.setFocusPainted(false);
+        newSupplier.setFont(new Font("Arial", Font.BOLD, 16));
+
+        newSupplier.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                newSupplier.setBackground(new Color(60,60,60));
+            }
+
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                newSupplier.setBackground(new Color(40,40,40));
+            }
+        });
+
+        newSupplier.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(e.getSource()==newSupplier)
+                {
+                    dispose();
+                    Admin n=new Admin();
+                    n.setVisible(true);
+                }
+
             }
         });
         newSupplier.addActionListener(new ActionListener() {
@@ -127,21 +195,134 @@ public class Supplier extends JFrame{
 // I stopped here
             }
         });
+        newSupplier.setBounds(250,390,100,50);
 
 
-        scroll.setBounds(450,65,500,150);
+        supplyProducts= new JButton("Restock");
+        supplyProducts.setBackground(new Color(40,40,40));
+        supplyProducts.setForeground(Color.WHITE);
+
+        supplyProducts.setFocusPainted(false);
+        supplyProducts.setFont(new Font("Arial", Font.BOLD, 16));
+
+        supplyProducts.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                supplyProducts.setBackground(new Color(60,60,60));
+            }
+
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                supplyProducts.setBackground(new Color(40,40,40));
+            }
+        });
+
+        supplyProducts.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(e.getSource()==supplyProducts)
+                {
+                    dispose();
+                    Admin n=new Admin();
+                    n.setVisible(true);
+                }
+
+            }
+        });
+        supplyProducts.setBounds(350,390,100,50);
+        removeSupplier=new JButton("Remove Supplier");
+        removeSupplier.setBackground(new Color(40,40,40));
+        removeSupplier.setForeground(Color.WHITE);
+
+        removeSupplier.setFocusPainted(false);
+        removeSupplier.setFont(new Font("Arial", Font.BOLD, 16));
+
+        removeSupplier.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                removeSupplier.setBackground(new Color(60,60,60));
+            }
+
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                removeSupplier.setBackground(new Color(40,40,40));
+            }
+        });
+
+        removeSupplier.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(e.getSource()==removeSupplier)
+                {
+                    dispose();
+                    Admin n=new Admin();
+                    n.setVisible(true);
+                }
+
+            }
+        });
+        removeSupplier.setBounds(450,390,100,50);
+        p1=new JPanel(null){
+            private BufferedImage backgroundImage;
+
+            {
+                try {
+
+                    backgroundImage = ImageIO.read(new File("C:/Users/mekon/IdeaProjects/SupermarketManagementSystem/greyImage.jpeg"));
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+
+                if (backgroundImage != null) {
+                    g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), null);
+                }
+            }
+        };
+
+        p2=new JPanel(null){
+            private BufferedImage backgroundImage;
+
+            {
+                try {
+
+                    backgroundImage = ImageIO.read(new File("C:/Users/mekon/IdeaProjects/SupermarketManagementSystem/greyImage.jpeg"));
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+
+                if (backgroundImage != null) {
+                    g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), null);
+                }
+            }
+        };
+
+        p2.setPreferredSize(new Dimension(600,400));
+
+        p1.setPreferredSize(new Dimension(600,50));
+
+
+
+
+        scroll.setBounds(50,15,700,350);
         scroll.setBackground(Color.WHITE);
-        newSupplier.setBounds(100,450,200,50);
-        supplyProducts.setBounds(400,450, 200, 50);
-        removeSupplier.setBounds(700,450, 200, 50);
-        Cb.setBounds(1,2,100,100);
-        Sup.add(newSupplier);
-        Sup.add(supplyProducts);
-        Sup.add(removeSupplier);
-        Sup.add(Cb);
-        Sup.add(tf);
-        Sup.add(tf2);
-        Sup.add(scroll);
+
+      //  p1.add(back);
+        p1.add(Cb);
+        p1.add(textField);
+        p1.add(back);
+        p2.add(scroll);
+        p2.add(removeSupplier);
+        p2.add(newSupplier);
+        p2.add(supplyProducts);
+        setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+        Sup.add(p1);
+        Sup.add(p2);
 
     }
 
