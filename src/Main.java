921eae20fb77d55e390c884898bf048fa73d0f64
Nahom Mekonnen <namek2729@gmail.com>
2023-09-13 00:22:04 +0300

    Third Commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import database.Database;
-import login.*;
 import employee.Employee;
 import inventory.Inventory;
+import supplier.Supplier;
 
 import javax.swing.*;
 
@@ -12,11 +12,19 @@ public class Main {
 //     Login l=new Login();
 //     l.setVisible(true);
 
-        Database database =new  Database("localhost","SuperMarket","sa","Nani2239");
+        Database database =new  Database("localhost","SuperMarket","sa","Nohom420");
         database.instantiate();
+//        Inventory inventory=new Inventory();
+//        inventory.setVisible(true);
+        Supplier supplier=new Supplier();
+        supplier.setVisible(true);
 
-        Login l=new Login();
-        l.setVisible(true);
+//        Admin ad=new Admin();
+//        ad.setVisible(true);
+///        Admin n=new Admin();
+//        n.setVisible(true);
+//        Employee e=new Employee();
+//        e.setVisible(true);
 
 
     }
--- src/supplier/SupplierActions.java
+++ src/supplier/SupplierActions.java
@@ -1,5 +1,104 @@
 package supplier;
 
-public class SupplierActions {
+import database.Database;
+import product.Product;
 
+
+import java.sql.*;
+
+public class SupplierActions {
+    int id;
+    String firstName,lastName,phoneNumber,itemSupplied,address;
+
+    void Add(Supplier supplier) throws SQLException {
+        try {
+
+            PreparedStatement statement= Database.connection.prepareStatement("Insert into suppliers(?,?,?,?,?,?)");
+            statement.setInt(1,supplier.getSupplier_id());
+            statement.setString(2,supplier.getFirstName());
+            statement.setString(3,supplier.getLastName());
+            statement.setString(4,supplier.getPhoneNumber());
+            statement.setString(5,supplier.getItemSupplied());
+            statement.setString(6,supplier.getAddress());
+            System.out.println("Added");
+            statement.executeUpdate();
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
+    }
+    void Supply(Product product){
+        try {
+
+            PreparedStatement statement= Database.connection.prepareStatement("Insert into products(?,?,?,?,?,?)");
+            statement.setInt(1,product.getProduct_id());
+            statement.setString(2,product.getProduct_name());
+            statement.setDouble(3,product.getPrice());
+            statement.setInt(4,product.getAmount());
+            statement.setDate(5, (Date) product.getDate());
+            statement.setInt(6,product.getCategory_id());
+            System.out.println("Added");
+            statement.executeUpdate();
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
+    }
+
+    int Count() {
+        int num = 0;
+        try {
+            Statement stmt = Database.connection.createStatement();
+            String query = "SELECT COUNT(supplier_id) FROM Suppliers";
+            ResultSet resultSet = stmt.executeQuery(query);
+            while (resultSet.next()){
+                return resultSet.getInt(1);
+            }
+            System.out.println(num);
+            return num;
+
+        } catch (Exception e) {
+            System.out.println(e);
+            return 0;
+        }
+    }
+
+
+    Supplier[] Retrieve(int count) {
+        int x=0;
+        Supplier[] suppliers=new Supplier[count];
+        try {
+            Statement statement = Database.connection.createStatement();
+            String query = "Select * From Suppliers";
+            ResultSet resultSet = statement.executeQuery(query);
+
+            System.out.println("Attempt");
+
+            System.out.println("result " + resultSet);
+
+
+            while (resultSet.next()) {
+
+
+                id = resultSet.getInt(1);
+                firstName = resultSet.getString(2);
+                lastName = resultSet.getString(3);
+                phoneNumber = resultSet.getString(4);
+                itemSupplied = resultSet.getString(5);
+                address = resultSet.getString(6);
+                System.out.println(id + " " + firstName + " " + lastName + " " + phoneNumber + " " +  address + " " + itemSupplied);
+
+                Supplier newSupply = new Supplier(id,firstName,lastName,phoneNumber,itemSupplied,address);
+                suppliers[x]=newSupply;
+                x++;
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return suppliers;
+    }
 }
